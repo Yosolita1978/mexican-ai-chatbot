cc16d9df11f546f17b1512131602f991cff32be7
Cristina Rodriguez <yosola@gmail.com>
2025-10-02 12:32:40 -0700

    Analytics for everyone

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <script defer data-domain="sazonbot.vercel.app" src="https://plausible.io/js/script.file-downloads.hash.outbound-links.pageview-props.revenue.tagged-events.js"></script>
+<script>window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }</script>
+
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
